69f5168300e99ab00890e7b82d2800090415fc25
QossayFarhan <B02210014@student.unimy.edu.my>
2022-07-20 21:50:24 +0800

    Create events for main admin functionality

---
+++ app/src/main/java/com/mdp/librarydelivery/View_delivery.java
@@ -0,0 +1,16 @@
+package com.mdp.librarydelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mdp.librarydelivery.R;
+
+public class View_delivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_delivery);
+    }
+}
---
+++ app/src/main/java/com/mdp/librarydelivery/view_users.java
@@ -0,0 +1,16 @@
+package com.mdp.librarydelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mdp.librarydelivery.R;
+
+public class view_users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_users);
+    }
+}
